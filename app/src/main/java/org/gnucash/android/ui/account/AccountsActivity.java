55ebb974add7e3e55bd4261313741ce4c3d6edde
Moshe <pnemonic@gmail.com>
2024-06-24 20:17:04 +0300

    Scheduled actions need context.

--- app/src/main/java/org/gnucash/android/ui/account/AccountsActivity.java
+++ app/src/main/java/org/gnucash/android/ui/account/AccountsActivity.java
@@ -54,8 +54,8 @@ import org.gnucash.android.app.GnuCashApplication;
 import org.gnucash.android.databinding.ActivityAccountsBinding;
 import org.gnucash.android.db.DatabaseSchema;
 import org.gnucash.android.db.adapter.AccountsDbAdapter;
-import org.gnucash.android.db.adapter.BooksDbAdapter;
 import org.gnucash.android.importer.ImportAsyncTask;
+import org.gnucash.android.service.ScheduledActionService;
 import org.gnucash.android.ui.common.BaseDrawerActivity;
 import org.gnucash.android.ui.common.FormActivity;
 import org.gnucash.android.ui.common.Refreshable;
@@ -334,7 +334,7 @@ public class AccountsActivity extends BaseDrawerActivity implements OnAccountCli
         if (hasNewFeatures()) {
             showWhatsNewDialog(this);
         }
-        GnuCashApplication.startScheduledActionExecutionService(this);
+        ScheduledActionService.schedulePeriodicActions(this);
         BackupManager.schedulePeriodicBackups(this);
     }
 
