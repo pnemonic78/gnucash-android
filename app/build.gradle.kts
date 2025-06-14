bbeb2b4b7b0b6f412f1701bf90d0143a865289dc
pnemonic78 <pnemonic@gmail.com>
2025-06-14 23:29:46 +0300

    app 2.6.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ plugins {
 
 val versionMajor = 2
 val versionMinor = 6
-val versionPatch = 9
-val versionBuild = 2
+val versionPatch = 10
+val versionBuild = 0
 
 val dropboxAppKey =
     (project.properties["RELEASE_DROPBOX_APP_KEY"] as String?) ?: "dhjh8ke9wf05948"
