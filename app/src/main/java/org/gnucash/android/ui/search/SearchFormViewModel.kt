baa248299b18f568939affd292b80b68fa5c19b9
Moshe W <pnemonic@gmail.com>
2025-10-19 22:39:07 +0300

    Search transactions - transaction criteria.

--- app/src/main/java/org/gnucash/android/ui/search/SearchFormViewModel.kt
+++ app/src/main/java/org/gnucash/android/ui/search/SearchFormViewModel.kt
@@ -5,33 +5,12 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import java.util.Calendar
 
 class SearchFormViewModel : ViewModel() {
     val form = SearchForm()
 
-    private val _query = MutableStateFlow<SearchForm?>(null)
-    val query: Flow<SearchForm?> = _query
-
-    fun setDescription(value: String?) {
-        form.description = value
-    }
-
-    fun setNotes(value: String?) {
-        form.notes = value
-    }
-
-    fun setMemo(value: String?) {
-        form.memo = value
-    }
-
-    fun setDateStart(value: Long?) {
-        form.dateMin = value
-    }
-
-    fun setDateEnd(value: Long?) {
-        form.dateMax = value
-    }
+    private val _query = MutableStateFlow<String?>(null)
+    val query: Flow<String?> = _query
 
     fun onSearchClicked() {
         showResultsPage()
@@ -39,35 +18,39 @@ class SearchFormViewModel : ViewModel() {
 
     private fun showResultsPage() {
         viewModelScope.launch {
-            _query.emit(form.copy())
+            _query.emit(form.buildSQL())
         }
     }
 
-    fun setDateStart(year: Int, month: Int, dayOfMonth: Int) {
-        val calendar = Calendar.getInstance().apply {
-            set(Calendar.YEAR, year)
-            set(Calendar.MONTH, month)
-            set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            set(Calendar.HOUR_OF_DAY, 0)
-            set(Calendar.MINUTE, 0)
-            set(Calendar.SECOND, 0)
-        }
-        setDateStart(calendar.timeInMillis)
-    }
-
-    fun setDateEnd(year: Int, month: Int, dayOfMonth: Int) {
-        val calendar = Calendar.getInstance().apply {
-            set(Calendar.YEAR, year)
-            set(Calendar.MONTH, month)
-            set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            set(Calendar.HOUR_OF_DAY, getMaximum(Calendar.HOUR_OF_DAY))
-            set(Calendar.MINUTE, getMaximum(Calendar.MINUTE))
-            set(Calendar.SECOND, getMaximum(Calendar.SECOND))
-        }
-        setDateEnd(calendar.timeInMillis)
+    fun onSearchShowed() {
+        _query.value = null
     }
 
     fun setComparison(value: ComparisonType) {
         form.comparisonType = value
+    }
+
+    fun addDescription(): SearchCriteria.Description {
+        return form.addDescription()
+    }
+
+    fun addNote(): SearchCriteria.Note {
+        return form.addNote()
+    }
+
+    fun addMemo(): SearchCriteria.Memo {
+        return form.addMemo()
+    }
+
+    fun addDate(): SearchCriteria.Date {
+        return form.addDate()
+    }
+
+    fun addNumeric(): SearchCriteria.Numeric {
+        return form.addNumeric()
+    }
+
+    fun remove(item: SearchCriteria) {
+        form.remove(item)
     }
 }
